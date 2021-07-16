fd6f6be8d6576ac9d89f2d6dab3fefca7e453240
simachri <marvin.christe@posteo.de>
2021-07-16 14:18:44 +0200

    fix(superheroAPI) Image payload too large

--- app/bot.py
+++ app/bot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 import logging
-from requests import ConnectionError
+from requests import HTTPError
 from config import read_from_env
 from superheroes import fetch_random_superhero_avatar
 
@@ -22,33 +22,51 @@ async def on_ready():
     logging.info("Bot is up and running.")
 
 
-@slash.slash(name="ping",
-             description="Check if the Bot is available.",
+@slash.slash(# The name of our slash command. It will become available as '/<name>' in Discord.
+             name="ping",
+             # A short description what the slash command does. It will be visible in 
+             # Discord.  
+             description="Check if the Bot responds.",
+             # We provide the guild_id of our Discord server. This is a little "trick"
+             # within the Discord API to make our slash command become available in 
+             # Discord right away.
              guild_ids=guild_ids)
 async def pong(ctx: SlashContext):
+    # Write an info log message to our console.
     logging.info("Received slash command /ping.")
-    embed = discord.Embed(title="Hello World!")
-    await ctx.send(content="Bot is up and running.", embeds=[embed])
+    # ctx.send sends the text back to Discord.
+    await ctx.send(content="Hey there, this is a message from your favourite Bot.")
 
 
 @slash.slash(name="superhero",
-             description="Make the bot become a superhero.",
+             description="Change the bot's avatar to a random superhero image.",
              guild_ids=guild_ids)
 async def change_avatar(ctx: SlashContext):
     """Change the bot's avatar to a random superhero."""
     logging.info("Received slash command /superhero.")
     try:
+        # Send an initial response to Discord that the command has been received.
+        # The initial response is required as the entire response might take longer
+        # than three seconds for processing. If that is the case and no initial response
+        # has been sent to Disocord, the interaction will fail.
+        # Calling ctx.defer() shows a "bot ... is thinking." message in Discord.
+        # See: https://discord.com/developers/docs/interactions/slash-commands#responding-to-an-interaction
+        await ctx.defer()
+        # Call the superhero API and receive a random superhero image.
         hero_name, avatar_bytes = await fetch_random_superhero_avatar()
         await bot.user.edit(avatar=bytearray(avatar_bytes.getvalue()))
-        file = discord.File(avatar_bytes, filename="newAvatar.png")
-        embed = discord.Embed()
-        embed.set_image(url="attachment://newAvatar.png")
-        await ctx.send(content=f'Bot became {hero_name}.',
-                       file=file, embed=embed)
+        # # Optional: Embed a larger image of the super hero into the Bot's reply.
+        # file = discord.File(avatar_bytes, filename="newAvatar.png")
+        # embed = discord.Embed()
+        # embed.set_image(url="attachment://newAvatar.png")
+        # await ctx.send(content=f'Bot became {hero_name}.',
+                       # file=file, embed=embed)
+        await ctx.send(content=f'Bot became {hero_name}.')
         logging.info(f"Changed the bot's avatar to {hero_name}.")
-    except ConnectionError:
-        logging.exception("Superhero API returned an error.")
-        await ctx.send("Calling the superhero API failed.")
+    except HTTPError as err:
+        logging.exception(f"Superhero API returned an error {err.response.text}.")
+        await ctx.send("Calling the superhero API failed. See the console log for details.")
 
 
+# Start the bot.
 bot.run(bot_token)
--- app/superheroes.py
+++ app/superheroes.py
@@ -1,33 +1,114 @@
 import requests
 from io import BytesIO
 import random
+import logging
 
+# Setup logging.
+logging.basicConfig(level=logging.INFO)
 
 async def fetch_random_superhero_avatar() -> tuple[str, BytesIO]:
     """Fetch a random superhero avatar from the API https://akabab.github.io/superhero-api/api/.
+
+      This is an example of the JSON data returned by the API when invoking /api/id/1.json:
+       {
+        "id": 1,
+        "name": "A-Bomb",
+        "slug": "1-a-bomb",
+        "powerstats": {
+            "intelligence": 38,
+            "strength": 100,
+            "speed": 17,
+            "durability": 80,
+            "power": 24,
+            "combat": 64
+        },
+        "appearance": {
+            "gender": "Male",
+            "race": "Human",
+            "height": [
+            "6'8",
+            "203 cm"
+            ],
+            "weight": [
+            "980 lb",
+            "441 kg"
+            ],
+            "eyeColor": "Yellow",
+            "hairColor": "No Hair"
+        },
+        "biography": {
+            "fullName": "Richard Milhouse Jones",
+            "alterEgos": "No alter egos found.",
+            "aliases": [
+            "Rick Jones"
+            ],
+            "placeOfBirth": "Scarsdale, Arizona",
+            "firstAppearance": "Hulk Vol 2 #2 (April, 2008) (as A-Bomb)",
+            "publisher": "Marvel Comics",
+            "alignment": "good"
+        },
+        "work": {
+            "occupation": "Musician, adventurer, author; formerly talk show host",
+            "base": "-"
+        },
+        "connections": {
+            "groupAffiliation": "Hulk Family; Excelsior (sponsor), Avengers (honorary member); formerly partner of the Hulk, Captain America and Captain Marvel; Teen Brigade; ally of Rom",
+            "relatives": "Marlo Chandler-Jones (wife); Polly (aunt); Mrs. Chandler (mother-in-law); Keith Chandler, Ray Chandler, three unidentified others (brothers-in-law); unidentified father (deceased); Jackie Shorr (alleged mother; unconfirmed)"
+        },
+        "images": {
+            "xs": "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/xs/1-a-bomb.jpg",
+            "sm": "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/1-a-bomb.jpg",
+            "md": "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/1-a-bomb.jpg",
+            "lg": "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/1-a-bomb.jpg"
+        }
+      }
 
       :returns:
         - name of avatar
         - avatar as bytes
       :raises:
-          requests.ConnectionError: Calling the API returned a non-200 error code.
+          HTTPError: Calling the API returned a non-200 error code.
     """
     # Use the cached URL of the API: https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api
     # The superhero database contains 731 entries, see https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json
-    # Generate a random superhero ID between 1 and 731.
-    hero_id = random.randint(1, 731)
-    response = requests.get(
-        f"https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/{hero_id}.json")
-    if response.status_code != 200:
-        raise requests.ConnectionError
+    hero_found = False
+    response = None
+    for _ in range(5):
+        # Generate a random superhero ID between 1 and 731.
+        hero_id = random.randint(1, 731)
+        logging.info(f"Random superhero ID is {hero_id}.")
+        response = requests.get(
+            f"https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/{hero_id}.json")
+        # For some hero IDs the data package is very large and the HTTP request returns
+        # a 403 error "Package size exceeds the configured limit of 50 MB".
+        # Check if this error is returned. If yes, query a different, yet less performant 
+        # URL.
+        if response.status_code == 200:
+            logging.info("Superhero data retrieved.")
+            hero_found = True
+            break
+        logging.info("No superhero data retrieved. Trying again.")
+    if not hero_found:
+        if response:
+            # Raise an exception if the HTTP request has a return code other than 200 - OK.
+            response.raise_for_status()
+        else:
+            raise requests.HTTPError("No superhero found.")
+
+    # Get the superhero data from the HTTP response.
     hero_data = response.json()
 
-    # Fetch the avatar image.
+    # Download the avatar image from the URL that is given in the superhero
+    # data under images/sm (= small image).
     hero_avatar_url = hero_data["images"]["sm"]
-    response = requests.get(hero_avatar_url, stream=True)
-    if response.status_code != 200:
-        raise requests.ConnectionError
+    logging.info(f"Superhero avatar image URL is {hero_avatar_url}.")
 
+    response = requests.get(hero_avatar_url, stream=True)
+    # Raise an exception if the HTTP request has a return code other than 200 - OK.
+    response.raise_for_status()
+
+    # Get the hero name.
     hero_name = hero_data["name"]
+    logging.info(f"Superhero name is {hero_name}.")
 
     return hero_name, BytesIO(response.content)
