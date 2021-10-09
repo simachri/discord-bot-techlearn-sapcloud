102f823cd0fd4e383f72f565a4047c98b4498083
simachri <marvin.christe@posteo.de>
2021-10-09 18:04:49 +0200

    chore: Clean up starter coding.

--- app/bot.py
+++ app/bot.py
@@ -1,18 +1,11 @@
 from discord.ext import commands
-from discord_slash import SlashCommand, SlashContext
 import logging
-from requests import HTTPError
 from config import read_from_env
-from superheroes import fetch_random_superhero_avatar
 
 # Read the configuration from environment variables.
-bot_token, guild_ids, log_level = read_from_env()
+bot_token, guild_ids, _ = read_from_env()
 
-# Activate application logging.
-logging.basicConfig(level=log_level)
-
-# Start the bot and make it connect to the Discord guild/server
-# through the bot_token.
+# Set up a new bot instance.
 bot = commands.Bot(command_prefix="")
 
 
@@ -22,4 +15,6 @@ async def on_ready():
     logging.info("Bot is up and running.")
 
 
+# Start the bot and make it connect to the Discord guild/server
+# through the bot_token.
 bot.run(bot_token)
